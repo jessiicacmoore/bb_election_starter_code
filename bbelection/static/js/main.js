38b708b5eaebb5895d4f5bd76f95186fa4e7838c
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-20 15:16:08 -0400

    Complete pt1

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+  const body = document.querySelector('body');
+
+  axios.get('https://bb-election-api.herokuapp.com/')
+    .then(resp => {
+      let data = resp.data.candidates;
+      let candidateList = document.createElement('ul');
+      body.appendChild(candidateList);
+
+      data.forEach(candidate => {
+        let candidateData = document.createElement('li');
+        candidateData.innerHTML = `<p><strong>Name: </strong>${candidate.name}</p>\n
+          <p><strong>Votes: </strong>${candidate.votes}</p>`;
+        candidateList.appendChild(candidateData);
+      })
+    });
 });
