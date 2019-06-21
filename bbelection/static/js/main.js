c9ae8d9e5bc9ae87a7492227a7fd0369fa4326b3
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-21 17:32:30 -0400

    Complete

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -11,8 +11,49 @@ document.addEventListener("DOMContentLoaded", function() {
       data.forEach(candidate => {
         let candidateData = document.createElement('li');
         candidateData.innerHTML = `<p><strong>Name: </strong>${candidate.name}</p>\n
-          <p><strong>Votes: </strong>${candidate.votes}</p>`;
+          <p><strong>Votes: </strong><span class="vote-count">
+          ${candidate.votes}</span></p>`;
         candidateList.appendChild(candidateData);
+
+        let voteForm = document.createElement('form');
+        voteForm.method = 'POST';
+        voteForm.action = 'https://bb-election-api.herokuapp.com/vote';
+        candidateData.appendChild(voteForm);
+
+        let voteInput = document.createElement('input');
+        voteInput.type = 'hidden';
+        voteInput.name = 'name';
+        voteInput.value = candidate.name;
+
+        let voteBtn = document.createElement('button');
+        voteBtn.innerText = `Vote for ${candidate.name}`;
+
+        voteForm.appendChild(voteInput);
+        voteForm.appendChild(voteBtn);
+
+        let refreshBtn = document.createElement('button');
+        refreshBtn.innerText = 'Refresh';
+        candidateData.appendChild(refreshBtn);
+        refreshBtn.addEventListener('click', () => {
+          axios.get('https://bb-election-api.herokuapp.com/')
+            .then(resp => {
+              let refreshedCandidates = resp.data.candidates;
+              let updateSpan = candidateData.querySelector('.vote-count');
+              updateSpan.innerText = refreshCandidates[data.indexOf(candidate)].votes;
+            })
+        })
       })
     });
+
+  document.addEventListener('submit', e => {
+    e.preventDefault();
+
+    axios.post(e.target.action, {
+      name : event.target.querySelector('input[name="name"').value
+    })
+      .then(resp => {
+        console.log(e.target.nextElementSibling);
+        e.target.nextElementSibling.click();
+      })
+  })
 });
